37056b9cffc7709dc9885c2d2b7ca0f57725ad4e
Apex Arcadian <joe456789rob@gmail.com>
2024-04-29 20:40:32 -0400

    adding preview.html, no thumbnails

--- js/listItems.js
+++ js/listItems.js
@@ -6,7 +6,10 @@ const ipAddresses = [
     '195.189.29.6:8181',
     '74.105.120.201:444',
     '92.100.144.74:8080',
-    '191.97.151.27:8080'
+    '191.97.151.27:8080',
+    '190.213.67.64:10001',
+    '139.64.168.120:8080',
+    '75.149.26.30:1024'
     // Add more IP addresses here...
 ];
 
@@ -15,33 +18,19 @@ async function fetchPreviewImages() {
     const ipContainer = document.getElementById('ip-container');
 
     for (const ip of ipAddresses) {
-        // try {
-        // `https://cors-anywhere.herokuapp.com/http://api.thumbnail.ws/api/ab95a5c79faf774a226af65072d81add4a5b50da17ff/thumbnail/get?url=http://${ip}/&width=800`
-        // const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.thumbnail.ws/api/ab95a5c79faf774a226af65072d81add4a5b50da17ff/thumbnail/get?url=http://${ip}/&width=800`);
-        // const data = await response.json();
-
-        // if (data && data.status === 'ok' && data.thumbnail) {
         const ipItem = document.createElement('div');
         ipItem.classList.add('ip-item');
 
-        // const img = document.createElement('img');
-        // img.src = `http://api.thumbnail.ws/api/ab95a5c79faf774a226af65072d81add4a5b50da17ff/thumbnail/get?url=${ip}/&width=800`;
-        // console.log(img.src);
-        // img.alt = `Preview of ${ip}`;
+        // we would like to also fetch images from the gh-repo thumnail database
+        // these are to be added at the administrator level
 
         const ipText = document.createElement('a');
         ipText.href = "http://" + ip;
         ipText.textContent = ip;
-
-        // ipItem.appendChild(img);
         ipItem.appendChild(ipText);
 
         ipContainer.appendChild(ipItem);
-        // } else {
-        // console.log(`No preview image found for ${ip}`);
     }
-    // } catch (error) {
-    // console.error(`Error fetching preview image for ${ip}:`, error);
 }
 
 // Fetch preview images when the page loads
