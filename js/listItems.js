2677eb8032a30eb79ee264b36fb1e45152dbc4d6
Apex Arcadian <joe456789rob@gmail.com>
2024-05-14 04:59:13 -0400

    more

--- js/listItems.js
+++ js/listItems.js
@@ -1,15 +1,16 @@
 // List of public IP addresses
 const ipAddresses = [
-    '1.34.165.233:8282',
-    '188.233.189.119:8085',
-    '116.121.216.151:8080',
-    '195.189.29.6:8181',
-    '74.105.120.201:444',
-    '92.100.144.74:8080',
-    '191.97.151.27:8080',
-    '190.213.67.64:10001',
-    '139.64.168.120:8080',
-    '75.149.26.30:1024'
+    'http://1.34.165.233:8282',
+    'http://188.233.189.119:8085',
+    'http://116.121.216.151:8080',
+    'http://195.189.29.6:8181',
+    'http://74.105.120.201:444',
+    'http://92.100.144.74:8080',
+    'http://191.97.151.27:8080',
+    'http://190.213.67.64:10001',
+    'http://139.64.168.120:8080',
+    'http://75.149.26.30:1024',
+    'http://185.14.139.118:81/view/index.shtml'
     // Add more IP addresses here...
 ];
 
@@ -25,7 +26,7 @@ async function fetchPreviewImages() {
         // these are to be added at the administrator level
 
         const ipText = document.createElement('a');
-        ipText.href = "http://" + ip;
+        ipText.href = ip;
         ipText.textContent = ip;
         ipItem.appendChild(ipText);
 
