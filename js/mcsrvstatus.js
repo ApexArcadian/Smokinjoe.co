7994175addb80f035d273d848bd955324ca83bbc
Apex Arcadian <joe456789rob@gmail.com>
2024-06-05 18:46:59 -0400

    test status panel

--- js/mcsrvstatus.js
+++ js/mcsrvstatus.js
@@ -1,12 +1,15 @@
-var url = "https:/api.minetools.eu/ping/join.smokinjoeco/23132";
-
+//based on a pen by @robinselmer
+var url = "https://api.mcsrvstat.us/debug/bedrock/join.smokinjoe.co/";
+ 
 $.getJSON(url, function(r) {
-    if(r.error) {
-        $('#rest').html('Server Offline');
-        return false;
-    }
-    var pl = '';
- if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
+    //data is the JSON string
+ if(r.error){
+    $('#rest').html('Server Offline');
+   return false;
+ } 
+var pl = '';
+ if(r.players.list.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
   $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
- $('#favicon').attr('src', r.favicon);
+ $('#favicon').attr('src', r.favicon); $('#rest').css('opacity', 1);
+    
 });
